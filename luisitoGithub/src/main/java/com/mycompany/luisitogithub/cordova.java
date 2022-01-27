62e58c65dea17cdef38844957c7870d39830c736
Pc1 <Pc1@PC1-LUIS>
2022-01-27 17:49:04 -0400

    Practica#4

--- luisitoGithub/src/main/java/com/mycompany/luisitogithub/cordova.java
+++ luisitoGithub/src/main/java/com/mycompany/luisitogithub/cordova.java
@@ -9,14 +9,30 @@ package com.mycompany.luisitogithub;
  * @author Pc1
  */
 public class cordova {
+    
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {  
+    public static void main(String[] args) { 
+        
+                                            // Variables de tipos enteros
+        byte entero = 10;                   //De -128 a 127
+        short num2 = 15000;                 //De -32.768 a 32767 
+        int num3 = 200000;                  //De -2.147,483,615 a 2,147,483,617
+        long num4 = 4000000;                //De -9,223,372,036,854,775,808 a 9,223,372,854,775,807
+        float decimal = 14.5f; 
+        double decimal2 = 19.5454545;
+        char caracter = 'L';
         
         System.out.println("Seguimos practicando"); 
-        System.out.println("Tercer prueba"); 
+        System.out.println("Tercer prueba"); // SOUT ATAJO PARA IMPRIMIR TEXTO
+       System.out.println("El numero es -->"+ num2);
+        System.out.println("El numero es -->"+entero);
+        System.out.println("EL numero es --->"+num3);
+        System.out.println("El numero decimal es -->"+decimal);
+        System.out.println("El numero decimal es ---->"+decimal2);
+        System.out.println("Caracter es -->"+caracter);
         // TODO code application logic here
     }
     
