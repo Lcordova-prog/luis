591616423b1b3fb6b29aa6ee6cb7b91faed2fbbe
Pc1 <Pc1@PC1-LUIS>
2022-01-26 23:58:02 -0400

    saludo

---
+++ luis/src/main/java/com/mycompany/luis/proyectoGit.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.luis;
+
+/**
+ *
+ * @author Pc1
+ */
+public class proyectoGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Que tal gente?");
+        // TODO code application logic here
+    }
+    
+}
